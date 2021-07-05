59e276b7b67eb383adc2b2e7961b25103832b1be
Hiroshi Ouchiyama <hiouchiy@outlook.jp>
2021-07-05 13:19:24 +0900

    Update

--- azure_vm/script/CUSTOM_SCRIPT_start_train.sh
+++ azure_vm/script/CUSTOM_SCRIPT_start_train.sh
@@ -65,27 +65,26 @@ TEXT="//${AZURE_STORAGE_ACCOUNT_NAME}.file.core.windows.net/train /mnt/train cif
 sudo bash -c "echo ""$TEXT"" >> /etc/fstab"
 sudo mount -t cifs //${AZURE_STORAGE_ACCOUNT_NAME}.file.core.windows.net/train /mnt/train -o vers=3.0,credentials=/etc/smbcredentials/${AZURE_STORAGE_ACCOUNT_NAME}.cred,dir_mode=0777,file_mode=0777,serverino &>> /home/ai/deploy.log
 
-JOB_ID_DIR=/mnt/logs/"$JOB_ID"
-mkdir $JOB_ID_DIR
-
 ##############################################
 # Download training data from Azure BLOB
 ##############################################
-mkdir /home/ai/data &>> $JOB_ID_DIR/train.log
-chmod 777 /home/ai/data &>> $JOB_ID_DIR/train.log
-cd /home/ai/data &>> $JOB_ID_DIR/train.log
-sudo apt-get update -y &>> $JOB_ID_DIR/train.log
-sudo apt-get install wget unzip -y &>> $JOB_ID_DIR/train.log
-cp /mnt/train/"$TRAIN_DATASET_URL" train.zip &>> $JOB_ID_DIR/train.log
-mkdir train &>> $JOB_ID_DIR/train.log
-unzip train.zip -d train &>> $JOB_ID_DIR/train.log
+mkdir /home/ai/data
+chmod 777 /home/ai/data
+cd /home/ai/data
+sudo apt-get update -y
+sudo apt-get install wget unzip -y
+cp /mnt/train/"$TRAIN_DATASET_URL" train.zip
+mkdir train
+unzip train.zip -d train
+JOB_ID_DIR=/mnt/logs/"$JOB_ID"
+mkdir $JOB_ID_DIR
 
 CONTINUOUS_TRAINING=false
 if [ $REFERED_JOB_ID != "nothing" ]; then
     if [ -d /mnt/logs/$REFERED_JOB_ID ]; then
         if [ -e /mnt/logs/$REFERED_JOB_ID/latest.pth ]; then
 		    echo "cp /mnt/logs/$REFERED_JOB_ID/latest.pth start.pth" &>> $JOB_ID_DIR/train.log
-            cp /mnt/logs/$REFERED_JOB_ID/latest.pth start.pth &>> $JOB_ID_DIR/train.log
+            cp /mnt/logs/$REFERED_JOB_ID/latest.pth start.pth
 			echo "CONTINUOUS_TRAINING=true" &>> $JOB_ID_DIR/train.log
             CONTINUOUS_TRAINING=true
 	    fi
@@ -95,20 +94,20 @@ fi
 ##############################################
 # Install Docker
 ##############################################
-sudo apt-get update -y &>> $JOB_ID_DIR/train.log
-sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common &>> $JOB_ID_DIR/train.log
-curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - &>> $JOB_ID_DIR/train.log
-sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable" &>> $JOB_ID_DIR/train.log
-sudo apt-get update &>> $JOB_ID_DIR/train.log
-apt-cache policy docker-ce &>> $JOB_ID_DIR/train.log
-sudo apt-get install -y docker-ce &>> $JOB_ID_DIR/train.log
+sudo apt-get update -y
+sudo apt-get install -y apt-transport-https ca-certificates curl software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add - 
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu bionic stable"
+sudo apt-get update
+apt-cache policy docker-ce
+sudo apt-get install -y docker-ce
 echo "Docker Installed" &>> $JOB_ID_DIR/train.log
 
 ###########################################################
 # Special Proc. GetIP address of ubuntu mirror in Japan
 ###########################################################
 APT_IP=`nslookup jp.archive.ubuntu.com | grep Address |  tail -n +2 | cut -f2 -d ' '`
-echo $APT_IP &>> $JOB_ID_DIR/train.log
+echo $APT_IP
 
 ##############################################
 # Data transformation
